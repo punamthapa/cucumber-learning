ad6082966eaf22a899d1c6e101509720d1621e72
punamthapa <punam.chhetri.372@gmail.com>
2023-05-19 11:11:56 +0545

    cypress test

---
+++ cypress/e2e/normal-script/login.cy.js
@@ -0,0 +1,15 @@
+/// <reference types= "cypress" />
+
+
+describe('Login Test',function(){
+    
+  it('success login',()=>{
+
+      cy.visit("https://www.saucedemo.com/")
+      cy.get("#user-name").type('standard_user')
+      cy.get("#password").type('secret_sauce')
+      cy.get("#login-button").click()
+      cy.url().should('contains','/inventory.html')
+
+  });
+})
